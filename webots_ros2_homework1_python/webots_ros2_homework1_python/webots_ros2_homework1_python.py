97b3171bc69f2bb1b9c551b8b7f04822bfb4f53c
klandrzejewski <klandrzejewski@crimson.ua.edu>
2024-10-03 15:57:06 -0500

    update speed

--- webots_ros2_homework1_python/webots_ros2_homework1_python/webots_ros2_homework1_python.py
+++ webots_ros2_homework1_python/webots_ros2_homework1_python/webots_ros2_homework1_python.py
@@ -141,8 +141,8 @@ class RandomWalk(Node):
             #self.cmd.linear.x = 0.150 # Move forward at trial speed
             self.cmd.linear.x = 0.0 
             #self.cmd.linear.z = 0.0
-            #self.cmd.angular.z = 0.5236 # Turn at trial angle (30)
-            self.cmd.angular.z = 2.094 # Turn at trial angle (120)
+            self.cmd.angular.z = 0.5236 # Turn at trial angle (30)
+            #self.cmd.angular.z = 2.094 # Turn at trial angle (120)
             self.publisher_.publish(self.cmd)
             self.turtlebot_moving = True
 
